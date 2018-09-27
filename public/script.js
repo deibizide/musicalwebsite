f2474fb44c7e749384f108d54ed7f5d3eb3dedb0
David Ramis <david.ramis89@gmail.com>
2018-09-27 16:56:01 +0200

    right-nav scroll blocked

--- public/script.js
+++ public/script.js
@@ -6,11 +6,10 @@ $(".hb-button").on("click", function(e) {
     var leftPx = parseInt($(".right-nav").css("margin-left"), 10);
     $(".right-nav").animate({ "margin-left": leftPx < 0 ? 0 : -200 }, "slow");
     $(".hb-button").animate({ "margin-left": leftPx < 0 ? 240 : 25 }, "slow");
-    $(".hb-button > i").toggleClass("hide");
+    // $(".hb-button > i").toggleClass("hide");
     $(".square-box").toggleClass("hide");
+    $("body").toggleClass("no-scrolling");
 });
-
-$("body").css({ "overflow-x": "hidden" });
 
 ///////////////////
 // SMOOTH SCROLL //
